f001a4415d6b4d0a5eda50761e9963a4922fa2e3
mansimishra123 <95296459+mansimishra123@users.noreply.github.com>
2022-06-07 17:36:38 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
 song1="";
 song2="";
+leftwristx=0;
+leftwristy=0;
+rightwristy=0;
+rightwristx=0;
 
 function preload(){
     song1=loadSound("music.mp3");
@@ -12,6 +16,31 @@ function setup(){
 
     video=createCapture(VIDEO);
 video.hide()
+
+poseNet=ml5.poseNet(video,modelloaded)
+
+poseNet.on('pose', gotposes)
+}
+
+
+function gotposes(result){
+
+    if(result.length>0){
+      
+        console.log(result)
+
+        leftwristx=result[0].pose.leftWrist.x;
+        leftwristy=result[0].pose.leftWrist.y;
+        console.log('leftwristx='+leftwristx+'leftwristy='+leftwristy)
+        rightwristy=result[0].pose.rightWrist.x;
+        rightwristy=result[0].pose.rightWrist.y;
+console.log('rightwristy='+rightwristy+'rightwristx='+rightwristy)
+
+    }
+}
+
+function modelloaded(){
+    console.log("posenet is Initialize")
 }
 
 function draw(){
